0e009af2d10723c6dc4bc749fca209b093371db6
Bibin Jacob Pulickal <bibinjacob123@gmail.com>
2020-05-18 17:28:59 +0530

    Added BBAlert and BBAlertPresentable protocol.

--- Sources/BBAlert/BBAlert.swift
+++ Sources/BBAlert/BBAlert.swift
@@ -1,3 +1,125 @@
-struct BBAlert {
-    var text = "Hello, World!"
+//
+//  BBAlert.swift
+//  BBAlert
+//
+//  Created by Bibin Jacob Pulickal on 17/05/20.
+//
+
+import UIKit
+
+public typealias BBAlert = UIAlertController
+
+public extension BBAlert {
+
+    convenience init(title: String = "", message: String = "") {
+        self.init(title: title, message: message, preferredStyle: .alert)
+
+        pruneNegativeWidthConstraints()
+    }
+
+    func pruneNegativeWidthConstraints() {
+        for subView in view.subviews {
+            for constraint in subView.constraints where constraint.debugDescription.contains("width == - 16") {
+                subView.removeConstraint(constraint)
+            }
+        }
+    }
+}
+
+public extension BBAlert {
+
+    var backgroundColor: UIColor? {
+        set {
+            view.subviews.first?.subviews.first?.subviews.first?.backgroundColor = newValue
+        }
+        get {
+            view.subviews.first?.subviews.first?.subviews.first?.backgroundColor
+        }
+    }
+
+    var textColor: UIColor? {
+        set {
+            titleTextColor   = newValue
+            messageTextColor = newValue
+        }
+        get {
+            titleTextColor
+        }
+    }
+
+    var titleTextColor: UIColor? {
+        set {
+            guard let foregroundColor = newValue else { return }
+            let attributedTitle = NSAttributedString(string: title ?? "", attributes: [.foregroundColor: foregroundColor])
+            setValue(attributedTitle, forKey: "attributedTitle")
+        }
+        get {
+            let attributedTitle = value(forKey: "attributedTitle") as? NSAttributedString
+            var color: UIColor?
+            for (key, value) in attributedTitle?.attributes(at: 0, effectiveRange: nil) ?? [:] {
+                if key == .foregroundColor, let value = value as? UIColor {
+                    color = value
+                }
+            }
+            return color
+        }
+    }
+
+    var messageTextColor: UIColor? {
+        set {
+            guard let foregroundColor = newValue else { return }
+            let attributedMessage = NSAttributedString(string: message ?? "", attributes: [.foregroundColor: foregroundColor])
+            setValue(attributedMessage, forKey: "attributedMessage")
+        }
+        get {
+            let attributedMessage = value(forKey: "attributedMessage") as? NSAttributedString
+            var color: UIColor?
+            for (key, value) in attributedMessage?.attributes(at: 0, effectiveRange: nil) ?? [:] {
+                if key == .foregroundColor, let value = value as? UIColor {
+                    color = value
+                }
+            }
+            return color
+        }
+    }
+
+    var titleFont: UIFont? {
+        set {
+            guard let font = newValue else { return }
+            let attributedMessage = NSAttributedString(string: message ?? "", attributes: [.font: font])
+            setValue(attributedMessage, forKey: "attributedTitle")
+        }
+        get {
+            let attributedMessage = value(forKey: "attributedTitle") as? NSAttributedString
+            var font: UIFont?
+            for (key, value) in attributedMessage?.attributes(at: 0, effectiveRange: nil) ?? [:] {
+                if key == .font, let value = value as? UIFont {
+                    font = value
+                }
+            }
+            return font
+        }
+    }
+
+    var messageFont: UIFont? {
+        set {
+            guard let font = newValue else { return }
+            let attributedMessage = NSAttributedString(string: message ?? "", attributes: [.font: font])
+            setValue(attributedMessage, forKey: "attributedMessage")
+        }
+        get {
+            let attributedMessage = value(forKey: "attributedMessage") as? NSAttributedString
+            var font: UIFont?
+            for (key, value) in attributedMessage?.attributes(at: 0, effectiveRange: nil) ?? [:] {
+                if key == .font, let value = value as? UIFont {
+                    font = value
+                }
+            }
+            return font
+        }
+    }
+
+    func dismiss(completion: (() -> Void)? = nil) {
+        dismiss(animated: true, completion: completion)
+    }
 }
---
+++ Sources/BBAlert/BBAlertPresentable.swift
@@ -0,0 +1,40 @@
+//
+//  BBAlertPresentable.swift
+//  BBAlert
+//
+//  Created by Bibin Jacob Pulickal on 17/05/20.
+//
+
+import UIKit
+
+public protocol BBAlertPresentable {
+
+    func present(_ viewControllerToPresent: UIViewController, completion: (() -> Void)?)
+    func present(_ viewControllerToPresent: UIViewController, animated flag: Bool, completion: (() -> Void)?)
+}
+
+extension UIViewController: BBAlertPresentable { }
+
+extension BBAlertPresentable {
+
+    public func present(_ viewControllerToPresent: UIViewController, completion: (() -> Void)?) {
+        present(viewControllerToPresent, animated: true, completion: completion)
+    }
+}
+
+extension BBAlertPresentable {
+
+    public func presentBBAlert(title: String, message: String, actions: [String: UIAlertAction.Style] = [:], setup: ((BBAlert) -> Void)? = nil, actionHandler: ((UIAlertAction) -> Void)? = nil, completion: ((BBAlert) -> Void)? = nil) {
+
+        let alert = BBAlert(title: title, message: message)
+
+        for (key, value) in actions {
+            alert.addAction(UIAlertAction(title: key, style: value, handler: actionHandler))
+        }
+        setup?(alert)
+
+        present(alert) {
+            completion?(alert)
+        }
+    }
+}
