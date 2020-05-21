cff08301a78a361bbac34222469d93267b8812d8
Bibin Jacob Pulickal <bibinjacob123@gmail.com>
2020-05-21 19:27:38 +0530

    Removed show method.

--- Sources/BBAlert/BBAlertPresentable.swift
+++ Sources/BBAlert/BBAlertPresentable.swift
@@ -37,21 +37,4 @@ extension BBAlertPresentable {
             completion?(alert)
         }
     }
-
-    public func showBBAlert(_ title: String? = "Error!",
-                   message: String?,
-                   actions: [String: UIAlertAction.Style] = [:],
-                   handler: ((UIAlertAction) -> Void)? = nil,
-                   setup: ((UIAlertController) -> Void)? = nil) {
-        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        if actions.isEmpty {
-            alert.addAction(UIAlertAction(title: "Okay", style: .default, handler: handler))
-        } else {
-            for action in actions {
-                alert.addAction(UIAlertAction(title: action.key, style: action.value, handler: handler))
-            }
-        }
-        setup?(alert)
-        present(alert)
-    }
 }
