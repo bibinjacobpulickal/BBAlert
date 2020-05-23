8fdac281d8f9ee9cf63d39486ae907497de6ab37
Bibin Jacob Pulickal <bibinjacob123@gmail.com>
2020-05-23 19:53:41 +0530

    Fixed closure implicit self error.

--- Sources/BBAlert/BBAlert.swift
+++ Sources/BBAlert/BBAlert.swift
@@ -29,7 +29,7 @@ public extension BBAlert {
 
         if !duration.isZero {
             DispatchQueue.main.asyncAfter(deadline: .now() + duration) {
-                dismiss(completion: completion)
+                self.dismiss(completion: completion)
             }
         }
     }
